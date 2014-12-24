e14bafa4a01470f90068e0250c6dedba19b4f3d5
Roi Ashkenazi <roia@wix.com>
2014-12-24 21:10:22 +0200

    initial

--- javascript/TennisGame1.js
+++ javascript/TennisGame1.js
@@ -3,6 +3,7 @@
 var Score = function() {
 	
 	this.score = 0;
+	this.scoreArray = ["Love", "Fifteen", "Thirty", "Forty"];
 	
 	this.increase = function() {
 		if (this.score <= 3) {
@@ -11,10 +12,26 @@ var Score = function() {
 	};
 	
 	this.isTie = function(other) {
-		return score === other.score;
+		return this.score === other.score;
+	};
+	
+	this.isWin = function(other) {
+		return this.score >= 4 && Math.abs(this.score - other.score) >= 2;
+	};
+
+	this.getTieText = function(n) {
+		switch (n) {
+			case 0: case 1: case 2: return this.scoreArray[n] + "-All";
+			default: return "Deuce";
+		};
 	};
 	
 	this.getScoreText = function(other) {
+		if (this.isTie(other)) {
+			return this.getTieText(this.score);
+		} 
+		
+		
 		return "zubi";
 	};
 	
@@ -26,7 +43,7 @@ var PlayerInfo = function(name) {
 };
 
 var calculateScore = function(player1, player2) {
-	return "zubi";
+	return player1.score.getScoreText(player2.score);
 };
 
 var TennisGame1 = function(player1Name, player2Name) {
