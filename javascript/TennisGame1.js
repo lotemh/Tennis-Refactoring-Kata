e42d88391ced627cbc0ec7bd4bb252d1633e03be
sharon kerzman <sharonk@wix.com>
2014-12-25 12:15:57 +0200

    self calling function was added

--- javascript/TennisGame1.js
+++ javascript/TennisGame1.js
@@ -1,3 +1,5 @@
+	
+var TennisGame1 = (function() {	
 	// defines player
 	function Player(pName) {
         this.name = pName;
@@ -18,22 +20,20 @@
 	
 	// defines tennis game
 	
-    var TennisGame1 = function (player1Name, player2Name) {
-        this.player1 = new Player(player1Name);
+	var TennisGame = function (player1Name, player2Name){
+	    this.player1 = new Player(player1Name);
         this.player2 = new Player(player2Name);
 		this.scoreNames = ["Love", "Fifteen", "Thirty", "Forty"];
-
-    };
-	
-	TennisGame1.prototype.getPlayerByName = function(name) {
+	};
+	TennisGame.prototype.getPlayerByName = function(name) {
         return (name === this.player1.getName()) ? this.player1 : this.player2;
     };
 
-    TennisGame1.prototype.wonPoint = function (name) {
+    TennisGame.prototype.wonPoint = function (name) {
         this.getPlayerByName(name).addPoint();
     };
 	
-	TennisGame1.prototype.calcAdvantage = function() {
+	TennisGame.prototype.calcAdvantage = function() {
             var scorePrefix = (Math.abs(this.player1score - this.player2score) === 1) ? "Advantage" : "Win for";
             var winner = (this.player1score > this.player2score) ? this.player1 : this.player2;
 			console.log(this.player1score , this.player2score);
@@ -42,7 +42,7 @@
             return scorePrefix + " " + winnerName;
         }
 	
-    TennisGame1.prototype.getScore = function () {
+    TennisGame.prototype.getScore = function () {
         this.player1score = this.player1.getPoints();
         this.player2score = this.player2.getPoints();
 
@@ -57,9 +57,12 @@
             score = this.scoreNames[this.player1score] + "-" + this.scoreNames[this.player2score];
         }
         return score;
+		
     };
-
-
+	    return function (player1Name, player2Name) {
+		return new TennisGame(player1Name, player2Name);
+    };
+})();
 if (typeof window === "undefined") {
     module.exports = TennisGame1;
 }
